835ddfcf705beff2140c8fdf46815ed0fd59bf7b
LwBane <emiliepessoa8@gmail.com>
2026-05-13 19:46:43 -0300

    delete

--- src/controllers/PatientController.ts
+++
@@ -1,3 +0,0 @@
-import type { Request, Response } from "express";
-import type { Paciente } from "../prisma/generated/prisma/client"
-// import { AuthService, authService } from "../services/AuthService";
--- src/services/PatientService.ts
+++
@@ -1,2 +0,0 @@
-import type { Paciente } from "../prisma/generated/prisma/client";
-// import { patientRepository, type PatientRepository } from "../repositories/PatientRepository";
